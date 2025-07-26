97e559506257ee8efccca70ae545ef807007c2a9
Vyacheslav <reach5@mail.ru>
2025-07-26 16:45:59 +0600

    added successfulRegistrationTest

---
+++ src/test/java/PracticeFormTest.java
@@ -0,0 +1,54 @@
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.appear;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+
+public class PracticeFormTest {
+
+    @BeforeAll()
+    static void beforeAll() {
+        Configuration.baseUrl = "https://demoqa.com";
+        Configuration.browserSize = "1920x1080";
+        Configuration.pageLoadStrategy = "eager";
+
+    }
+
+    @Test
+    void successfulRegistrationTest() {
+        String userName = "Oleg";
+
+        open("/automation-practice-form");
+        $(".practice-form-wrapper").shouldHave(text("Practice Form"));
+        executeJavaScript("$('#fixedban').remove()");
+
+        $("#firstName").setValue(userName);
+        $("#lastName").setValue("Olegov");
+        $("#userEmail").setValue("olegolegovich@gmail.com");
+        $("#userNumber").setValue("1234567890");
+        $("#genterWrapper").$(byText("Other")).click(); // best
+        $("#dateOfBirthInput").click();
+        $(".react-datepicker__month-select").selectOption("May");
+        $(".react-datepicker__year-select").selectOption("2010");
+        $(".react-datepicker__day--030:not(.react-datepicker__day--outside-month)").click();
+        $("#subjectsInput").setValue("Math").pressEnter();
+        $("#hobbiesWrapper").$(byText("Music")).click();
+        $("#uploadPicture").uploadFromClasspath("img/1.png");
+        $("#currentAddress").setValue("Some adrdress 1");
+        $("#state").click();
+        $("#stateCity-wrapper").$(byText("NCR")).click();
+        $("#city").click();
+        $("#stateCity-wrapper").$(byText("Delhi")).click();
+        $("#submit").click();
+        $(".modal-dialog").should(appear);
+        $("#example-modal-sizes-title-lg").shouldHave(text("Thanks for submitting the form"));
+        $(".table-responsive").shouldHave(text(userName), text("Olegov"),
+                text("olegolegovich@gmail.com"), text("1234567890"), text("Music"));
+
+
+    }
+}
